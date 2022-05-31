b455840f8d057e82c28a4a7b90be17521764f71a
katherine rueda <kathkien24@gmail.com>
2022-05-30 19:03:07 -0600

    second commit console

--- array-reduceRight.js
+++ array-reduceRight.js
@@ -1,7 +1,6 @@
 //Resta los números en la matriz, comenzando desde el final:
 const numbers = [175, 50, 25];
-
-document.getElementById("demo").innerHTML = numbers.reduceRight(myFunc);
+console.log(numbers.reduceRight(myFunc));
 
 function myFunc(total, num) {
     return total - num;
@@ -9,7 +8,7 @@ function myFunc(total, num) {
 
 //Resta los números, de derecha a izquierda, y muestra la suma:
 const numbers = [2, 45, 30, 100];
-document.getElementById("demo").innerHTML = numbers.reduceRight(getSum);
+console.log(numbers.reduceRight(getSum));
 
 function getSum(total, num) {
     return total - num;
